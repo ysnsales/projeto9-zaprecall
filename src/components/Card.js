6174ecab5380c7d028fd8e76f29b51abe7cc8428
Yasmin <ysnsaless@gmail.com>
2023-03-04 21:19:10 -0300

    virar perguntas e respostas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,125 @@
 import styled from "styled-components"
+import { useState } from "react"
 
+export default function Card(props) {
 
-export default function Card() {
+    const [pergunta, setPergunta] = useState(false);
+    const [resposta, setResposta] = useState(false)
+
     return (
         <>
-            <BoxCard>
-                <p>Pergunta 1</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </BoxCard>
+            {resposta ?
+                <RespostaCard>
+                    <p>{props.card.answer}</p>
+                    <div>
+                        <button style={{background: "#FF3030"} }>Não lembrei</button>
+                        <button style={{background: "#FF922E"}}>Quase não lembrei</button>
+                        <button style={{background: "#2FBE34"}}>Zap!</button>
+                    </div>
+                </RespostaCard>
 
-            <BoxCard>
-                <p>Pergunta 2</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </BoxCard>
+                :
+                (pergunta ?
+                    <PerguntaCard>
+                        <p>{props.card.question}</p>
+                        <div><img src="./assets/seta_virar.png" alt="seta" onClick={() => setResposta(true)} /> </div>
+                    </PerguntaCard>
+                    :
+                    <FlashCard>
+                        <p>Pergunta {props.i + 1}</p>
+                        <ion-icon name="play-outline" onClick={() => setPergunta(true)} ></ion-icon>
+                    </FlashCard>)
 
-            <BoxCard>
-                <p>Pergunta 3</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </BoxCard>
-
-            <BoxCard>
-                <p>Pergunta 4</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </BoxCard>
-
-            <BoxCard>
-                <p>Pergunta 5</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </BoxCard>
-
-            
+            }
         </>
-
     )
+
+
 }
 
-const BoxCard = styled.div`
-min-height: 65px;
+const RespostaCard = styled.div`
+height: 100px;
+width: 80%;
+margin: 17px auto;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 15px;
+
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+background-color: #FFFFD4;
+
+    p {
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        color: #333333;
+
+    }
+    div {
+        margin: 0 auto;
+    }
+    button {
+        width: 85px;
+        height: 37px;
+        border-radius: 5px;
+        border: 0;
+        margin: 0 4px;
+
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        color: #FFFFFF;
+    }
+`
+const PerguntaCard = styled.div`
+height: 101px;
+width: 80%;
+margin: 17px auto;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+padding: 15px;
+
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+background-color: #FFFFD4;
+;
+
+    p {
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+        color: #333333;
+    }
+
+    div {
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    img {
+        align-self: flex-end;
+    }
+
+`
+const FlashCard = styled.div`
+height: 35px;
 width: 80%;
 margin: 17px auto;
 display: flex;
 flex-direction: row;
 align-items: center;
 justify-content: space-between;
+padding: 15px;
 
 box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
 border-radius: 5px;
@@ -55,14 +132,11 @@ background-color: white;
         font-size: 16px;
         line-height: 19px;
         color: #333333;
-
-        margin-left: 15px;
     }
 
     ion-icon {
         width: 20px;
         height: 23px;
-        margin-right: 15px;
     }
 
 `
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,11 +1,34 @@
 import Card from "./Card"
 import styled from "styled-components"
+import { useState } from "react"
+
 
 
 export default function Deck() {
-    return(
+
+    const cards = [
+        { question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
+        { question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+        { question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+        { question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+        { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+        { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+        { question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
+        { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+    ]
+
+    return (
         <DeckCard>
-        <Card />
+            {cards.map((card, i) => (
+                <Card
+                    key={i}
+                    i={i}
+                    card={card}
+                    cards={cards}
+                />
+                    ))
+            }
+
         </DeckCard>
     )
 }
@@ -14,3 +37,4 @@ const DeckCard = styled.div`
 margin-bottom: 97px;
 
 `
+
